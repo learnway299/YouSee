864057db8d126f0eafad236e739987193a50c19b
learnway299 <humanweakless@gmail.com>
2020-12-11 22:15:11 +0800

    Commit info

--- app/src/main/java/edu/hubu/yousee/settingFragment.java
+++ app/src/main/java/edu/hubu/yousee/settingFragment.java
@@ -7,6 +7,15 @@ import android.view.ViewGroup;
 
 import android.app.Fragment;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link settingFragment#newInstance} factory method to
@@ -14,6 +23,10 @@ import android.app.Fragment;
  */
 public class settingFragment extends Fragment {
 
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+    private MyLocationListener myListener = new MyLocationListener();
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
@@ -58,6 +71,71 @@ public class settingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.demo04, container, false);
+       // return inflater.inflate(R.layout.demo04, container, false);
+
+        View view = inflater.inflate(R.layout.demo04, container, false);
+        mMapView = (MapView) view.findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+        //定位初始化
+        mLocationClient = new LocationClient(getActivity());
+        mLocationClient.registerLocationListener(myListener);
+        //注册监听函数
+
+        //通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        option.setIsNeedLocationDescribe(true);//获得当前点的位置信息，此处必须为true
+
+        //设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+        //注册LocationListener监听器
+        MyLocationListener myLocationListener = new MyLocationListener();
+        mLocationClient.registerLocationListener(myLocationListener);
+        //开启地图定位图层
+        mLocationClient.start();
+        return view;
+    }
+
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection()).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            String locationDescribe = location.getLocationDescribe();    //获取位置描述信息
+        }
+    }
+
+
+    @Override
+    public void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
     }
 }
--- app/src/main/java/edu/hubu/yousee/youseeFragment.java
+++ app/src/main/java/edu/hubu/yousee/youseeFragment.java
@@ -2,19 +2,44 @@ package edu.hubu.yousee;
 
 import android.app.Fragment;
 
+import android.database.Cursor;
 import android.os.Bundle;
 
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link youseeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class youseeFragment extends Fragment {
+public class youseeFragment extends Fragment implements View.OnClickListener {
+    private View view;
+    private MyDAO myDAO;  //数据库访问对象
+    private ListView listView;
+    private List<Map<String,Object>> listData;
+    private Map<String,Object> listItem;
+    private SimpleAdapter listAdapter;
+
+    private EditText et_name;  //数据表包含3个字段，第1字段为自增长类型
+    private EditText et_price;
+
+    private  String selId=null;  //选择项id
+
 
 //    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +85,87 @@ public class youseeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.demo01, container, false);
+        view = inflater.inflate(R.layout.demo01, container, false);
+        Button bt_add= (Button) view.findViewById(R.id.bt_add);bt_add.setOnClickListener(this);
+        Button bt_modify=(Button) view.findViewById(R.id.bt_modify);bt_modify.setOnClickListener(this);
+        Button bt_del=(Button) view.findViewById(R.id.bt_del);bt_del.setOnClickListener(this);
+
+        et_name=(EditText) view.findViewById(R.id.et_name);
+        et_price=(EditText) view.findViewById(R.id.et_price);
+
+        myDAO = new MyDAO(getActivity());  //创建数据库访问对象
+        if(myDAO.getRecordsNumber()==0) {  //防止重复运行时重复插入记录
+            myDAO.insertInfo("tian", 20);   //插入记录
+            myDAO.insertInfo("wang", 40); //插入记录
+        }
+
+        displayRecords();   //显示记录
+        return view;
     }
 
+    public void displayRecords(){  //显示记录方法定义
+        listView = (ListView)view.findViewById(R.id.listView);
+        listData = new ArrayList<Map<String,Object>>();
+        Cursor cursor = myDAO.allQuery();
+        while (cursor.moveToNext()){
+            int id=cursor.getInt(0);  //获取字段值
+            String name=cursor.getString(1);
+            //int age=cursor.getInt(2);
+            int price=cursor.getInt(cursor.getColumnIndex("price"));//推荐此种方式
+            listItem=new HashMap<String,Object>(); //必须在循环体里新建
+            listItem.put("_id", id);  //第1参数为键名，第2参数为键值
+            listItem.put("name", name);
+            listItem.put("price", price);
+            listData.add(listItem);   //添加一条记录
+        }
+        listAdapter = new SimpleAdapter(getActivity(),
+                listData,
+                R.layout.item_layout, //自行创建的列表项布局
+                new String[]{"_id","name","price"},
+                new int[]{R.id.tv_id,R.id.tvname,R.id.tvprice});
+        listView.setAdapter(listAdapter);  //应用适配器
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {  //列表项监听
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String,Object> rec= (Map<String, Object>) listAdapter.getItem(position);  //从适配器取记录
+                et_name.setText(rec.get("name").toString());  //刷新文本框
+                et_price.setText(rec.get("price").toString());
+                Log.i("ly",rec.get("_id").toString());
+                selId=rec.get("_id").toString();  //供修改和删除时使用
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {  //实现的接口方法
+        if(selId!=null) {  //选择了列表项后，可以增加/删除/修改
+            String p1 = et_name.getText().toString().trim();
+            int p2 = Integer.parseInt(et_price.getText().toString());
+            switch (v.getId()){
+                case  R.id.bt_add:
+                    myDAO.insertInfo(p1,p2);
+                    break;
+                case  R.id.bt_modify:
+                    myDAO.updateInfo(p1,p2,selId);
+                    Toast.makeText(getActivity(),"更新成功！",Toast.LENGTH_SHORT).show();
+                    break;
+                case  R.id.bt_del:
+                    myDAO.deleteInfo(selId);
+                    Toast.makeText(getActivity(),"删除成功！",Toast.LENGTH_SHORT).show();
+                    et_name.setText(null);et_price.setText(null); selId=null; //提示
+            }
+        }else{  //未选择列表项
+            if(v.getId()==R.id.bt_add) {  //单击添加按钮
+                String p1 = et_name.getText().toString();
+                String p2=et_price.getText().toString();
+                if(p1.equals("")||p2.equals("")){  //要求输入了信息
+                    Toast.makeText(getActivity(),"name和price都不能空！",Toast.LENGTH_SHORT).show();
+                }else{
+                    myDAO.insertInfo(p1, Integer.parseInt(p2));  //第2参数转型
+                }
+            } else{   //单击了修改或删除按钮
+                Toast.makeText(getActivity(),"请先选择记录！", Toast.LENGTH_SHORT).show();
+            }
+        }
+        displayRecords();//刷新ListView对象
+    }
 }
